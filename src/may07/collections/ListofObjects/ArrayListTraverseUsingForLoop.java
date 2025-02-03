60493a2bfd5f50b36454b8ae75be3758bec8bc24
myGitHub2244 <jaip22@yahoo.com>
2025-02-02 19:33:59 -0500

    updated

---
+++ src/may07/collections/ListofObjects/ArrayListTraverseUsingForLoop.java
@@ -0,0 +1,37 @@
+package may07.collections.ListofObjects;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListTraverseUsingForLoop {
+
+    public static void main(String[] args) {
+
+            List<Integer> list1=new ArrayList<>();
+            list1.add(10);
+            list1.add(20);
+            list1.add(30);
+            list1.add(40);
+
+        //Print the ArrayList
+        System.out.println("list1 = " + list1);
+
+        System.out.println("Traverse ArrayList using For Loop");
+        //Traverse the ArrayList using for loop:
+        for(int x=0;x<list1.size();x++){
+            System.out.println("Element: "+list1.get(x));
+        }
+
+        System.out.println("Traverse ArrayList using For-Each Loop");
+        //Traverse the ArrayList using for-each loop:
+        for(Integer element : list1){
+            System.out.println("Element: "+ element);
+        }
+
+
+    }
+
+
+}
